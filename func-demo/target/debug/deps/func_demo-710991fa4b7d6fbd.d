3f50947b690e978abc2365d71cbbf43be97f28b2
vannvan <862670198@qq.com>
2023-08-10 17:56:30 +0800

    update

--- func-demo/target/debug/deps/func_demo-710991fa4b7d6fbd.d
+++
@@ -1,5 +0,0 @@
-/Users/vannvan/Documents/vannvan@github/rust-learn/func-demo/target/debug/deps/func_demo-710991fa4b7d6fbd: src/main.rs
-
-/Users/vannvan/Documents/vannvan@github/rust-learn/func-demo/target/debug/deps/func_demo-710991fa4b7d6fbd.d: src/main.rs
-
-src/main.rs:
--- func-demo/target/debug/func-demo.d
+++
@@ -1,1 +0,0 @@
-/Users/vannvan/Documents/vannvan@github/rust-learn/func-demo/target/debug/func-demo: /Users/vannvan/Documents/vannvan@github/rust-learn/func-demo/src/main.rs
