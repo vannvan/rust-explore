4a1a9b9e2e4ed93666dc68413aa0def23610608a
vannvan <862670198@qq.com>
2023-08-19 23:34:49 +0800

    request

--- client/README.md
+++ client/README.md
@@ -1,1 +1,5 @@
 # 接口请求
+
+## 测试接口
+
+- <https://api.vvhan.com/api/horoscope?type=scorpio&time=today> 每日星座运势API接口
--- client/src/main.rs
+++ client/src/main.rs
@@ -1,21 +1,47 @@
-use error_chain::error_chain;
-use std::io::Read;
+use serde::Deserialize;
+// #![deny(warnings)]
 
-error_chain! {
-    foreign_links {
-        Io(std::io::Error);
-        HttpRequest(reqwest::Error);
-    }
+#[derive(Deserialize, Debug)]
+#[allow(dead_code)] // 允许dead_code
+struct Response<T> {
+    success: bool,
+    data: T,
 }
 
-fn main() -> Result<()> {
-    let mut res = reqwest::blocking::get("http://httpbin.org/get")?;
-    let mut body = String::new();
-    res.read_to_string(&mut body)?;
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
+    // // 从入参接收
+    // let url = if let Some(url) = std::env::args().nth(1) {
+    //     url
+    // } else {
+    //     let default = String::from("https://api.vvhan.com/api/horoscope?type=scorpio&time=today");
+    //     println!("没有传入URL，使用默认链接了.");
+    //     default.into()
+    // };
 
-    println!("Status: {}", res.status());
-    println!("Headers:\n{:#?}", res.headers());
-    println!("Body:\n{}", body);
+    let url = String::from("https://api.vvhan.com/api/horoscope?type=scorpio&time=today");
+
+    eprintln!("Fetching {:?}...", url);
+
+    // let res = reqwest::get(url).await?.json::<Response>().await?;
+
+    // let _char: Response<char> = Response('a');
+    #[derive(Deserialize, Debug)]
+    struct Data {
+        title: String,
+        time: String,
+    }
+
+    let res = reqwest::get(url).await?.json::<Response<Data>>().await?;
+
+    // eprintln!("Response: {:?} {}", res.version(), res.status());
+    // eprintln!("Headers: {:#?}\n", res.headers());
+
+    // let body = res.text().await?;
+
+    println!("{},{},{}", res.success, res.data.time, res.data.title);
+
+    println!("{:?}", res);
 
     Ok(())
 }
