eb4e3a0f8e7aacf1a35c853abd247bcd93944f1b
vannvan <862670198@qq.com>
2023-09-12 11:33:34 +0800

    update

--- exec-git/src/main.rs
+++ exec-git/src/main.rs
@@ -1,17 +1,28 @@
+use std::io::{BufRead, BufReader};
+use std::process::{self, Command, Stdio};
 fn main() {
-    // let err = exec::Command::new("echo").arg("hello").arg("world").exec();
+    let pull = Command::new("git")
+        .arg("pull")
+        .output()
+        .expect("pull执行异常，提示");
+    println!("{}", String::from_utf8(pull.stdout).unwrap());
 
-    //  let info = exec::Command::new("git add .").exec();
+    let add = Command::new("git")
+        .args(&["add", "."])
+        .output()
+        .expect("add执行异常，提示");
 
-    let status = exec::Command::new("git").arg("status").exec();
+    println!("{}", String::from_utf8(add.stdout).unwrap());
 
-    let pull = exec::Command::new("git").arg("pull").exec();
+    let status = Command::new("git")
+        .arg("status")
+        .output()
+        .expect("status执行异常，提示");
+    println!("{}", String::from_utf8(status.stdout).unwrap());
 
-    let add = exec::Command::new("git").arg("add .").exec();
-
-    // let commit = exec::Command::new("git").arg("commit -m update").exec();
-
-    // let push = exec::Command::new("git").arg("push").exec();
-
-    println!("status: {},pull: {}", status, pull);
+    let commit = Command::new("git")
+        .args(&["commit", "-m", "update"])
+        .output()
+        .expect("commit执行异常，提示");
+    println!("{}", String::from_utf8(commit.stdout).unwrap());
 }
