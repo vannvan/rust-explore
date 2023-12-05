db3dcfd905811648bc81ab2f90ad10f7e7af6dcf
vannvan <862670198@qq.com>
2023-12-05 18:07:52 +0800

    test: changelog info

--- tauri-todo-app/src/App.tsx
+++ tauri-todo-app/src/App.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import { invoke } from '@tauri-apps/api/tauri'
 import './App.css'
@@ -8,22 +8,47 @@ function App() {
   const [greetMsg, setGreetMsg] = useState('')
   const [name, setName] = useState('')
 
+  const [form] = Form.useForm()
+
+  useEffect(() => {
+    // 获取容器元素
+    const container = document.getElementById('container')
+
+    // 添加事件监听器
+    container!.addEventListener('contextmenu', function (event) {
+      // 阻止默认的右键点击行为
+      event.preventDefault()
+    })
+  }, [])
+
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
     setGreetMsg(await invoke('greet', { name }))
   }
 
+  const onFinish = (values: any) => {
+    console.log(values)
+  }
+
   return (
-    <div className="container">
-      <Form layout="inline">
-        <Form.Item label="事项" name="thingName">
+    <div className="container" id="container">
+      <Form layout="inline" form={form} onFinish={onFinish}>
+        <Form.Item label="事项" name="taskName">
           <Input />
         </Form.Item>
         <Form.Item label="截止时间" name="deadline">
-          <TimePicker />
+          <TimePicker style={{ width: 200 }} />
         </Form.Item>
         <Form.Item>
-          <Button type="primary">添加</Button>
+          <Button
+            type="primary"
+            htmlType="submit"
+            onClick={() => {
+              console.log(form.getFieldsValue())
+            }}
+          >
+            添加
+          </Button>
         </Form.Item>
       </Form>
     </div>
